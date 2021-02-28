bc95a5813d5d49f967f549b0fc69d74cfef76c40
Omar Choudhry <sc20osc@leeds.ac.uk>
2021-02-28 18:11:30 +0000

    Removed Initial Coursework 1 Files

--- coursework/cwk1/app/src/main/java/comp1721/cwk1/ActiveCases.java
+++
@@ -1,5 +0,0 @@
-package comp1721.cwk1;
-
-public class ActiveCases {
-  // Implement program for full solution here
-}
--- coursework/cwk1/chart/src/main/java/comp1721/cwk1/CovidChart.java
+++
@@ -1,4 +0,0 @@
-package comp1721.cwk1;
-
-// Implement your solution to the Advanced task here
-// (Note: the class must be named CovidChart)
