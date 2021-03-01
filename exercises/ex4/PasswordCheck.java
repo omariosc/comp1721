d4e9f7cb2378bbfc4c1f7735867d8d16de22ec0f
Omar Choudhry <sc20osc@leeds.ac.uk>
2021-03-01 15:44:51 +0000

    Implemented checks as separate static methods Completed Exercise

--- exercises/ex4/PasswordCheck.java
+++ exercises/ex4/PasswordCheck.java
@@ -7,37 +7,54 @@ import java.util.Scanner;
 class PasswordCheck {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in); // creates the scanner
+    System.out.println("Input password: "); // outputs prompt
     String password = input.nextLine(); // stores input password
 
-    // counter variables
-    int digits = 0;
-    int upper = 0;
-    int lower = 0;
-
-    for (char c : password.toCharArray()) { // iterate through password
-      if (Character.isDigit(c)) digits++; // increments if string contains digit
-      if (Character.isLowerCase(c)) lower++; // increments if string contains lower case character
-      if (Character.isUpperCase(c)) upper++; // increments if string contains upper case character
-    }
-
-    if (password.length() < 10) { // if string contains less than 10 characters
-      System.out.println("Error: password should contain at least 10 characters."); // error message
-      System.exit(1); // exits program
-    }
-    if (digits < 2) { // if string contains less than 2 digits
-      System.out.println("Error: password should contain at least two digits."); // error message
-      System.exit(1); // exits program
-    }
-    if (upper < 2) { // if string contains less than 2 digits
-      System.out.println("Error: password should contain at least two upper case characters."); // error message
-      System.exit(1); // exits program
-    }
-    if (lower < 2) { // if string contains less than 2 digits
-      System.out.println("Error: password should contain at least two lower case characters."); // error message
-      System.exit(1); // exits program
-    }
+    // runs all checks
+    digitCheck(password);
+    lowerCheck(password);
+    upperCheck(password);
+    characterCheck(password);
+    
+    // if all checks are valid
     System.out.println("Success: password valid.");
 
     input.close(); // closes the scanner
   } // end of main
+  public static void digitCheck(String str) { // checks number of digits in string
+    int counter = 0; // counter variable for digits
+    for (char c : str.toCharArray()) { // iterate through password
+      if (Character.isDigit(c)) counter++; // increments if string contains digit
+    }
+    if (counter < 2) { // if string contains less than 2 digits
+      System.out.println("Error: password should contain at least two digits."); // error message
+      System.exit(1); // exits program
+    }
+  } // end of digitCheck
+  public static void lowerCheck(String str) { // checks number of lower case characters in string
+    int counter = 0; // counter variable for lower case characters
+    for (char c : str.toCharArray()) { // iterate through password
+      if (Character.isLowerCase(c)) counter++; // increments if string contains lower case character
+    }
+    if (counter < 2) { // if string contains less than 2 digits
+      System.out.println("Error: password should contain at least two lower case characters."); // error message
+      System.exit(1); // exits program
+    }
+  } // end of lowerCheck
+  public static void upperCheck(String str) { // checks number of upper case characters in string
+    int counter = 0; // counter variable for upper case characters
+    for (char c : str.toCharArray()) { // iterate through password
+      if (Character.isUpperCase(c)) counter++; // increments if string contains upper case character
+    }
+    if (counter < 2) { // if string contains less than 2 digits
+      System.out.println("Error: password should contain at least two upper case characters."); // error message
+      System.exit(1); // exits program
+    }
+  } // end of upperCheck
+  public static void characterCheck(String str) {  // checks number of characters in string
+    if (str.length() < 10) { // if string contains less than 10 characters
+      System.out.println("Error: password should contain at least 10 characters."); // error message
+      System.exit(1); // exits program
+    }
+  } // end of characterCheck
 } // end of class PasswordCheck
