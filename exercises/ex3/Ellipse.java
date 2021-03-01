9113144ec949137ef636edce2f180781875ef7ea
Omar Choudhry <sc20osc@leeds.ac.uk>
2021-03-01 00:47:29 +0000

    Error if major length is smaller than minor length Error is inputs are less than or equal to 0 Completed exercise

--- exercises/ex3/Ellipse.java
+++ exercises/ex3/Ellipse.java
@@ -11,6 +11,22 @@ class Ellipse {
     double alpha = input.nextDouble(); // stores user input for first length
     System.out.print("Enter semi-minor axis length (smaller length): "); // outputs prompt for second length
     double beta = input.nextDouble(); // stores user input for second length
+
+    if (alpha < beta) { // if major length is smaller than minor length
+      System.out.println("Error: semi-major axis must be longer than semi-minor axis"); // outputs error message
+      System.exit(1); // exits program
+    }
+
+    if (alpha <= 0) { // if major length is 0
+      System.out.println("Error: semi-major axis length must be > 0"); // outputs error message
+      System.exit(1); // exits program
+    }
+
+    if (beta <= 0) { // if minor length is 0
+      System.out.println("Error: semi-minor axis length must be > 0"); // outputs error message
+      System.exit(1); // exits program
+    }    
+
     double area = alpha*beta*Math.PI; // computes area
     double eccentricity = Math.sqrt(1-((beta*beta)/(alpha*alpha))); // computes eccentricity
     System.out.printf("The area of the ellipse is = %1.3f%n", area); // outputs area
