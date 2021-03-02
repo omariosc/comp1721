01601e9513f8689100a74d726b6801e9b6f80d40
Omar Choudhry <sc20osc@leeds.ac.uk>
2021-03-02 12:51:51 +0000

    Created private array to represent data Created private string to store header Implemented size() Implemeneted getRecord() Implemeneted addRecord() Implemeneted readDailyCases() Implemeneted writeActiveCases()

--- coursework/cwk1/core/src/main/java/comp1721/cwk1/CovidDataset.java
+++ coursework/cwk1/core/src/main/java/comp1721/cwk1/CovidDataset.java
@@ -2,8 +2,19 @@
 package comp1721.cwk1;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CovidDataset {
+
+  // private field to represent array of data
+  private List<CaseRecord> data = new ArrayList<>();
+  private String[] header;
 
   public CovidDataset() {
 
@@ -13,33 +24,42 @@ public class CovidDataset {
    * @return Size of the dataset
    */
   public int size() {
-    return 0;
+    return data.size();
   }
 
   /**
+   * Returns a CaseRecord object stored at the given position index.
+   * 
    * @param index Specific index
    * @return CaseRecord given specific index
+   * @throws DatasetException
    */
-  public CaseRecord getRecord(int index) {
-    return null;
+  public CaseRecord getRecord(int index) throws DatasetException {
+    if (index >= 0 && index < size()) {
+      return data.get(index);
+    }
+    else {
+      throw new DatasetException("Error: index is invalid");
+    }
   }
 
   /**
-   * Adds a record.
+   * Appends given CaseRecord object to end of current sequence of stored records.
    * 
    * @param rec CaseRecord to add
    */
   public void addRecord(CaseRecord rec) {
-    
+    data.add(rec);
   }
 
   /**
    * Retrieves case record given a specific day.
    * 
    * @param day Specific day
-   * @return CaseRecord relating to specific dat
+   * @return CaseRecord relating to specific day
+   * @throws DatasetException
    */
-  public CaseRecord dailyCasesOn(LocalDate day) {
+  public CaseRecord dailyCasesOn(LocalDate day) throws DatasetException {
     return null;
   }
 
@@ -47,18 +67,52 @@ public class CovidDataset {
    * Reads all cases in the file.
    * 
    * @param filename Name of file to read from
+   * @throws IOException
    */
-  public void readDailyCases(String filename) {
-
+  public void readDailyCases(String filename) throws IOException {
+    BufferedReader reader = null;
+    try {
+      reader = new BufferedReader(new FileReader(filename));
+      String line;
+      header = reader.readLine().split(","); // reads header line
+      while ((line = reader.readLine()) != null) {
+        String[] fields = line.split(",");  
+        LocalDate date = LocalDate.parse(fields[0]);
+        int staffCases = Integer.parseInt(fields[1]);
+        int studentCases = Integer.parseInt(fields[2]);
+        int otherCases = Integer.parseInt(fields[3]);
+        CaseRecord record = new CaseRecord(date, staffCases, studentCases, otherCases);
+        data.add(record);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      if (reader != null) reader.close();
+    }
   }
 
   /**
    * Writes all cases to the file
    * 
    * @param filename Name of file to write to
+   * @throws IOException
    */
-  public void writeActiveCases(String filename) {
-
+  public void writeActiveCases(String filename) throws IOException {
+    BufferedWriter writer = null;
+    StringBuilder sb = new StringBuilder();
+    try {
+      writer = new BufferedWriter(new FileWriter(filename));
+      sb.append(header);
+      for (CaseRecord element : data) {
+        sb.append(element);
+        sb.append(",");
+      }
+      writer.write(sb.toString());
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      if (writer != null) writer.close();
+    }
   }
 
 } // end of class CovidDataset
