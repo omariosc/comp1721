b11cd20c57ac3e37f96593eddbad08a76c0f49cd
Omar Choudhry <sc20osc@leeds.ac.uk>
2021-03-02 10:33:34 +0000

    Initial stub for all methods

--- coursework/cwk1/core/src/main/java/comp1721/cwk1/CovidDataset.java
+++ coursework/cwk1/core/src/main/java/comp1721/cwk1/CovidDataset.java
@@ -1,15 +1,42 @@
+// Class to represent a Covid Dataset
 package comp1721.cwk1;
 
+import java.time.LocalDate;
+
 public class CovidDataset {
+
+  public CovidDataset() {
+
+  }
+
   // TODO: Write stub for size()
+  public int size() {
+    return 0;
+  }
 
   // TODO: Write stub for getRecord()
+  public CaseRecord getRecord(int index) {
+    return null;
+  }
 
   // TODO: Write stub for addRecord()
+  public void addRecord(CaseRecord rec) {
+    
+  }
 
   // TODO: Write stub for dailyCasesOn()
+  public CaseRecord dailyCasesOn(LocalDate day) {
+    return null;
+  }
 
   // TODO: Write stub for readDailyCases()
+  public void readDailyCases(String filename) {
+
+  }
 
   // TODO: Write stub for writeActiveCases()
-}
+  public void writeActiveCases(String filename) {
+
+  }
+
+} // end of class CovidDataset
