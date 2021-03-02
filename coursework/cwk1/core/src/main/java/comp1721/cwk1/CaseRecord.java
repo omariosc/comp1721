cbdf9f0d27d63b9db422a43a0497ad9034f196d1
Omar Choudhry <sc20osc@leeds.ac.uk>
2021-03-02 10:17:39 +0000

    Private fields to represent case components Added comments for all methods Stubs for four getter methods Stub for totalCases() Stub for toString()

--- coursework/cwk1/core/src/main/java/comp1721/cwk1/CaseRecord.java
+++ coursework/cwk1/core/src/main/java/comp1721/cwk1/CaseRecord.java
@@ -1,16 +1,84 @@
+// Class represents a Case Record
+// Composition with CovidDataset
+
 package comp1721.cwk1;
 
 import java.time.LocalDate;
+import java.time.format.*;
 
 public class CaseRecord {
-  // TODO: Write stub for constructor
-  public CaseRecord(LocalDate date, int staff, int student, int other) {
+  
+  // private fields to represent date and number of cases for case
+  private LocalDate date;
+  private int staffCases;
+  private int studentCases;
+  private int otherCases;
+
+  /**
+   * Creates a Case Record using date and cases for staff, student and others.
+   *
+   * @param d Value for date of case
+   * @param staff Value for number of staff cases
+   * @param student Value for number of student cases
+   * @param other Value for number of other cases
+   */
+  public CaseRecord(LocalDate d, int staff, int student, int other) {
     
   }
 
-  // TODO: Write stubs for four getter methods
+  /**
+   * @return Date component of case record
+   */
+  public LocalDate getDate() {
+    // return date;
+    return null;
+  }
 
-  // TODO: Write stub for totalCases()
+  /**
+   * @return Staff cases component of case record
+   */
+  public int getStaffCases() {
+    // return staffCases;
+    return 0;
+  }
+
+  /**
+   * @return Student cases component of case record
+   */
+  public int getStudentCases() {
+    // return studentCases;
+    return 0;
+  }
+
+  /**
+   * @return Other cases component of case record
+   */
+  public int getOtherCases() {
+    // return otherCases;
+    return 0;
+  }
   
-  // TODO: Write stub for toString()
+  /**
+   * @return Total number of cases
+   */
+  public int totalCases() {
+    // return staffCases + studentCases + otherCases;
+    return 0;
+  }
+  
+  /**
+   * Generates a string representation of this case.
+   *
+   * <p>The format is YYYY-MM-DD - i.e., 4 digits for year, 2 digits for month and day
+   * with dashes as separators.</p>
+   *
+   * @return Case record as a string
+   */
+  @Override
+  public String toString() {
+    // date.format(DateTimeFormatter.ofPattern("yyyy-mm-dd"));
+    // return String.format("%s: %d staff, %d students, %d other", date, staffCases, studentCases, otherCases);
+    return null;
+  }
+
 }
