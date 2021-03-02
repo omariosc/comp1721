9c991bfd2925678a05e94660502061de24584ffa
Omar Choudhry <sc20osc@leeds.ac.uk>
2021-03-02 18:28:29 +0000

    Checks for two command line arguments Catches exceptions that occuw with the data files Caught exceptions cause program to terminate with error code 2 Reads daily case data from csv file Writes active cases to different csv file

--- coursework/cwk1/app/src/main/java/comp1721/cwk1/ActiveCases.java
+++ coursework/cwk1/app/src/main/java/comp1721/cwk1/ActiveCases.java
@@ -1,5 +1,38 @@
+// Tests CovidDataset class
+// reads daily cases and writes active cases
+
 package comp1721.cwk1;
 
+import java.io.IOException;
+
 public class ActiveCases {
-  // Implement program for full solution here
+  public static void main(String[] args) {
+
+    // requires 2 command line arguments
+    if (args.length != 2) {
+      // usage message
+      System.err.println("Usage: java ActiveCases <filename> <filename>");
+      // terminates program
+      System.exit(2);
+    }
+
+    CovidDataset dataset = new CovidDataset();
+    
+    try {
+      // reads daily cases from first file
+      dataset.readDailyCases(args[0]);
+      // writes daily cases to second file
+      dataset.writeActiveCases(args[1]);
+    } catch (IOException error) {
+      // outputs error message
+      System.err.println("Error: cannot access files");
+      // terminates program
+      System.exit(2);
+    } catch (Exception error) {
+      // outputs error message
+      System.err.println(error);
+      // terminates program
+      System.exit(2);
+    }
+  }
 }
