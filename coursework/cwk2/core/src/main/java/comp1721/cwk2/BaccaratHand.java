0e179fec688dc0748b9b520bcca0a77a4cacaab6
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-14 23:09:41 +0100

    Implemented method stubs

--- coursework/cwk2/core/src/main/java/comp1721/cwk2/BaccaratHand.java
+++ coursework/cwk2/core/src/main/java/comp1721/cwk2/BaccaratHand.java
@@ -1,3 +1,32 @@
 package comp1721.cwk2;
 
-// Implement the BaccaratHand class here
+/**
+ * Representation of a Baccarat hand.
+ * Implementation takes use of CardCollection.java
+ */
+public class BaccaratHand {
+  public BaccaratHand() {
+
+  }
+
+  public int size() {
+    return 0;
+  }
+
+  public void add(Card card) {
+    
+  }
+
+  public int value() {
+    return 0;
+  }
+
+  public boolean isNatural() {
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}
