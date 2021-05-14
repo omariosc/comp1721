c3ffc77fdd023c325ce512ac4ee0ed8c69fe5b57
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-14 23:12:32 +0100

    Implemented method stubs

--- coursework/cwk2/core/src/main/java/comp1721/cwk2/BaccaratCard.java
+++ coursework/cwk2/core/src/main/java/comp1721/cwk2/BaccaratCard.java
@@ -1,3 +1,46 @@
 package comp1721.cwk2;
 
-// Implement the BaccaratCard class here
+/**
+ * Representation of a Baccarat playing card.
+ * Implementation takes use of Card.java
+ */
+public class BaccaratCard extends Card {
+
+  // Private fields to represent the rank and suit of a Baccaratcard
+  private Rank rank;
+  private Suit suit;
+
+  public BaccaratCard(Rank r, Suit s) {
+    super(r, s);
+  }
+
+  @Override
+  public final Rank getRank() {
+    return null;
+  }
+
+  @Override
+  public final Suit getSuit() {
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return null;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    return true;
+  }
+
+  @Override
+  public final int compareTo(Card other) {
+    return 0;
+  }
+
+  @Override
+  public final int value() {
+    return 0;
+  }
+}
