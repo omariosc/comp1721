f7512fcc238de517d83c7f7bcd0adf331174d47c
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-15 23:09:14 +0100

    Implemented all methods Uses string builder for toString

--- coursework/cwk2/core/src/main/java/comp1721/cwk2/BaccaratHand.java
+++ coursework/cwk2/core/src/main/java/comp1721/cwk2/BaccaratHand.java
@@ -1,32 +1,85 @@
 package comp1721.cwk2;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Representation of a Baccarat hand.
  * Implementation takes use of CardCollection.java
  */
 public class BaccaratHand {
+
+  // Private list to represent the BaccaratHand
+  protected List<BaccaratCard> hand;
+
+  /**
+   * Initial BaccaratHand object (linked list).
+   * Starts out as empty.
+   */
   public BaccaratHand() {
-
+    hand = new LinkedList<>();
   }
 
+  /**
+   * Provides the size of the Baccarat hand.
+   * 
+   * @return Number of cards in the hand
+   */
   public int size() {
-    return 0;
+    return hand.size();
   }
 
+  /**
+   * Adds BaccaratCard objects to the BaccaratHand.
+   * 
+   * @param card BaccaratCard to be added
+   */
   public void add(Card card) {
-    
+    hand.add((BaccaratCard) card);
   }
 
+  /**
+   * Calculates the points value of a hand in the game of Bacarrat.
+   * 
+   * @return Points value.
+   */
   public int value() {
-    return 0;
+    var value = 0;
+    // Iterates through cards in hand
+    for (BaccaratCard card : hand) {
+      value += card.value();
+    }
+    return value % 10;
   }
 
+  /**
+   * Returns boolean value based on points value of the Baccarat hand.
+   * 
+   * @return true if the hand has a points value of 8 or 9, false otherwise
+   */
   public boolean isNatural() {
-    return true;
+    return value() == 8 || value() == 9;
   }
 
+  /**
+   * Creates a complete string representation of this Bacarrat hand.
+   *
+   * <p>String contains two-character representations of each card,
+   * separated from each other by a space.</p>
+   *
+   * @return String representation of this Bacarrat hand
+   */
   @Override
   public String toString() {
-    return super.toString();
+    var bld = new StringBuilder();
+    for (BaccaratCard card : hand) {
+      bld.append(card.toString());
+      bld.append(" ");
+    }
+    var string = bld.toString();
+    if (string.length() > 0) {
+      string = string.substring(0, string.length() - 1);
+    }
+    return string;
   }
 }
