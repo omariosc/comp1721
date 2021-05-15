7eff85f7b9e0ef5cbcdf60e5d06c713eb2897d26
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-16 00:07:53 +0100

    Creates and intialises baccarat hands and shoe Shuffles shoe Deals 2 cards to both player and banker Stores string representation of each hand Calculates value for each hand Outputs hands Calculates if either hand are nautral and outputs appropriate response message

--- coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
+++ coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
@@ -1,5 +1,36 @@
 package comp1721.cwk2;
 
 public class Baccarat {
-  // Implement your Intermediate or Full solution in this class
+  public static void main(String[] args) {
+
+    // Creates and initialises Baccarat hands and shoe
+    var playerHand = new BaccaratHand();
+    var bankerHand = new BaccaratHand();
+    var baccaratShoe = new Shoe(6);
+
+    // Shuffles shoe and deals two cards to both player and banker
+    baccaratShoe.shuffle();
+    playerHand.add(baccaratShoe.deal());
+    playerHand.add(baccaratShoe.deal());
+    bankerHand.add(baccaratShoe.deal());
+    bankerHand.add(baccaratShoe.deal());
+    
+    // Stores string representation of each hand
+    var playerString = playerHand.toString();
+    var bankerString = bankerHand.toString();
+
+    // Calculates value for hands
+    var playerValue = playerHand.value();
+    var bankerValue = bankerHand.value();
+
+    // Outputs hands and message if either or both hands are naturals    
+    System.out.printf("Player: %s = %d%n", playerString, playerValue);
+    System.out.printf("Banker: %s = %d%n", bankerString, bankerValue);
+    if (playerHand.isNatural()) {
+      System.out.println("Player has a Natural");
+    }
+    if (bankerHand.isNatural()) {
+      System.out.println("Banker has a Natural");
+    } 
+  }
 }
