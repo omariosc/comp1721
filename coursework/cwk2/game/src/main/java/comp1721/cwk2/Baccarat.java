ff157e0e545274dd579e697d40e4494bcc74e5a1
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-17 00:31:00 +0100

    Completed Full solution Added note for running task through VSCode Added more comments Method to process round tie Method to check if either hand are natural Checks if either player or banker should receive third card Method to implement player's rule Method to implement banker's rule Method to implement the final scoring mechanism Added natural check and rulings into main Still error when retrieving user input through gradle task

--- coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
+++ coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
@@ -1,10 +1,17 @@
+/**
+ * Note: when running the :game:interactive gradle task through VSCode
+ * the build fails with an NoSuchElementException however this
+ * does not happen when running the task through the command line
+ */
+
 package comp1721.cwk2;
 
 import java.util.Scanner;
+import comp1721.cwk2.Card.Rank;
 
 public class Baccarat {
 
-  // Declares Baccarat hands and shoe
+  // Baccarat hands and shoe
   private static BaccaratHand playerHand;
   private static BaccaratHand bankerHand;
   private static Shoe baccaratShoe;
@@ -51,24 +58,23 @@ public class Baccarat {
    *    <code>baccaratShoe = new Shoe(6);</code>
    * 
    * Meaning that the game will not be able to use 8 decks
-   * However, there are two ways to let the user specify the number of decks:
+   * However, there are two ways to let the user specify the number of decks:</p>
    * 
-   * 1. User specified number of decks in the command line (processing any errors too)
+   * <p>1. User specified number of decks in the command line (processing any errors too)
    * 
-   *    <code>if (args != null && args[1] != null & (args[1].equals("6") || args[1].equals("8"))
+   *    <code>if (args[1].equals("6") || args[1].equals("8") 
    *            { baccaratShoe = new Shoe(args[1]); }</code>
    * 
-   * 2. Prompt user for input for the number of the decks in interactive mode
-   *    Use hard coded value in default mode
+   * <p>2. Prompt user for input for the number of the decks in interactive mode
+   *    Use hard coded value in default mode</p>
    * 
-   * I decided to go with implementation number 2 in which the user
+   * <p>I decided to go with implementation number 2 in which the user
    * chooses the number of decks they would like to play with in interactive mode,
-   * since in the default mode, no command line arguments are passed (as per coursework specification)
-   * and the functionality is essentialy the same.
+   * since in the default mode, no command line arguments are passed (per coursework specification)
+   * and the functionality is essentialy the same.</p>
    * 
-   * Therefore in default mode, the shoe contains 6 decks. Of course 
+   * <p>Therefore in default mode, the shoe contains 6 decks. Of course 
    * this has its own limitations in that in default mode the user cannot play with 8 decks.
-   * 
    * This option can be disabled by commenting out the <code>while</code> loop</p>
    */
   private static void initialiseShoe() {
@@ -77,21 +83,26 @@ public class Baccarat {
     // If interactive mode is on
     while (interactiveMode == 1) {
       // Outputs number of decks prompt
-      System.out.print("Enter number of decks you would like to play with (6/8): ");
+      System.out.print("Number of decks? (6/8): ");
       // Stores user input
       String userInput = input.next();
       // Checks if input is valid number of decks
       if (userInput.equals("6") || userInput.equals("8")) {
         // Sets number of decks to userInput
         decks = Integer.parseInt(userInput);
+        // Ends while loop
         break;
       }
       // If user supplied an invalid input
       else {
-        System.out.println("Please enter a valid input.");
+        // Outputs error message
+        System.out.println("Please enter a valid number of decks.");
       }
     }
+    // Initialises shoe with number of decks (hardcoded or user supplied)
     baccaratShoe = new Shoe(decks);
+    // Shuffles shoe
+    baccaratShoe.shuffle();
   }
 
   /**
@@ -114,19 +125,29 @@ public class Baccarat {
    * Displays the round number message and clears hand
    */
   private static void newRound() {
+    // Increments round number
     rounds++;
+    // Outputs round number
     System.out.printf("%nRound %d%n", rounds);
+    // Clears player and banker hands
     playerHand.discard();
     bankerHand.discard();
   }
 
   /**
    * Deals two cards to player and banker
+   * 
+   * <p>Cards are dealt starting from player and alternate between the hands
+   * (as per Wikipedia article)</p>
    */
   private static void dealCards() {
+    // 1st player card
     playerHand.add(baccaratShoe.deal());
-    playerHand.add(baccaratShoe.deal());
+    // 1st banker card
     bankerHand.add(baccaratShoe.deal());
+    // 2nd player card
+    playerHand.add(baccaratShoe.deal());
+    // 2nd banker card
     bankerHand.add(baccaratShoe.deal());
   }
 
@@ -137,6 +158,16 @@ public class Baccarat {
     // Outputs hands and message if either or both hands are naturals
     System.out.printf("Player: %s = %d%n", playerHand.toString(), playerHand.value());
     System.out.printf("Banker: %s = %d%n", bankerHand.toString(), bankerHand.value());
+  }
+
+  /**
+   * Processes round tie
+   */
+  private static void roundTie() {
+    // Increments number of rounds tied
+    roundsTies++;
+    // Outputs round tied message
+    System.out.println("Round tied!");
   }
 
   /**
@@ -160,6 +191,51 @@ public class Baccarat {
   }
 
   /**
+   * Checks if either hand are natural.
+   * 
+   * <p>If both hands are natural then round is tied.
+   * If player hand is natural and banker hand is not, then player wins.
+   * If banker hand in natural and player hand is not, then banker wins.</p>
+   * 
+   * <p>Also checks if both cards have value of 6 or 7.
+   * If both hands total 6, then round is tied.
+   * If both hands total 7, then round is tied.
+   * If one hand is 6 and the other hand is 7, then the higher hand wins.</p>
+   * 
+   * @return 1 if round ends, 0 if round continues
+   */
+  private static int naturalCheck() {
+    // Checks if both players have natural hands or both hands have same value of 6 or 7
+    if (playerHand.isNatural() && bankerHand.isNatural() ||
+    ((playerHand.value() == 6 && bankerHand.value() == 6) ||
+    (playerHand.value() == 7 && bankerHand.value() == 7))) {
+      // Round ties
+      roundTie();
+      // Round end
+      return 1;
+    }
+    // If player hand is natural or player scores 7 and banker scores 6
+    else if (playerHand.isNatural() || (playerHand.value() == 7 && bankerHand.value() == 6)) { 
+      // Player wins
+      playerWin();
+      // Round end
+      return 1; 
+    }
+    // If banker hand is natural or banker scores 7 and player scores 6
+    else if (bankerHand.isNatural() || (playerHand.value() == 6 && bankerHand.value() == 7)) {
+      // Banker wins
+      bankerWin();
+      // Round end
+      return 1;
+    }
+    // If neither hand was a natural
+    else {
+      // Round continues
+      return 0;
+    }
+  }
+
+  /**
    * Deals third card to player and outputs dealing message
    */
   private static void dealThirdCardPlayer() {
@@ -180,12 +256,93 @@ public class Baccarat {
   }
 
   /**
+   * Player's rule (as per Wikipedia article)
+   * 
+   * <p>If the player has an initial total of 0-5, they draw a third card.
+   * If the player has an initial total of 6 or 7, they stand</p>
+   */
+  private static void playersRule() {
+    // If player's hand value is 5 or less
+    if (playerHand.value() <= 5) {
+      // Deal third card to player
+      dealThirdCardPlayer();
+    }
+  }
+
+  /**
+   * Banker's rule (as per Wikipedia article)
+   * 
+   * <p>If the player stood pat (i.e., has only two cards), the banker regards only their own hand
+   * and acts according to the same rule as the player, that is the banker draws a third card with 
+   * hands 0–5 and stands with 6 or 7.</p>
+   * 
+   * If the player drew a third card, the banker acts according to following rules:
+   * <ul><li>If banker's total is <= 2, then banker draws third card</li>
+   * <li>If banker's total is 3, banker draws third card unless player's third card was an 8.</li>
+   * <li>If banker's total is 4, banker draws third card if player's third card was a 2-7.</li>
+   * <li>If banker's total is 5, banker draws third card if player's third card was a 4-7.</li>
+   * <li>If banker's total is 6, banker draws third card if player's third card was a 6 or 7.</li>
+   * <li>If banker's total is 7, banker stands.</li></ul>
+   */
+  private static void bankersRule() {
+    // If player stood pat and banker's hand value is 5 or less
+    if (playerHand.value() > 5 && bankerHand.value() <= 5) {
+      // Banker draws a card
+      dealThirdCardBanker();
+    }
+    // If the player drew a third card (using player's rule)
+    if (playerHand.value() <= 5) {
+      // Stores the player's third card
+      var thirdCard = playerHand.get(2);
+      // Stores the player's third card rank
+      var thirdCardRank = thirdCard.getRank();
+      // Computes if banker should have third card according to more complex rules
+      if (bankerHand.value() <= 2 ||
+      (bankerHand.value() < 7 &&
+      (thirdCardRank.equals(Rank.SEVEN) || thirdCardRank.equals(Rank.SIX)))
+      || (bankerHand.value() < 6 &&
+      (thirdCardRank.equals(Rank.FIVE) || thirdCardRank.equals(Rank.FOUR)))
+      || (bankerHand.value() < 5 &&
+      (thirdCardRank.equals(Rank.THREE) || thirdCardRank.equals(Rank.TWO))) 
+      || (bankerHand.value() == 3 && !(thirdCardRank.equals(Rank.EIGHT)))) {
+        // Banker draws a card
+        dealThirdCardBanker();
+      }
+    }
+  }
+
+  /**
+   * Checks for the score of each hand
+   * 
+   * <p>If both hands have the same value then round is tied
+   * If player hand is higher value than banker hand, then player wins
+   * If banker hand is higher value than player hand, then banker wins</p>
+   */
+  private static void finalScoring() {
+    // Checks if both players have same value
+    if (playerHand.value() == bankerHand.value()) {
+      // Round is tied
+      roundTie();
+    }
+    // If player hand is natural
+    else if (playerHand.value() > bankerHand.value()) { 
+      // Player wins
+      playerWin();
+    }
+    // If banker hand is natural
+    else if (playerHand.value() < bankerHand.value()) {
+      // Banker wins
+      bankerWin();
+    }
+  }
+
+  /**
    * Outputs message for round check and processes user input
    * Continues until user inputs valid input
    * 
    * @return 1 if user inputs "y", 0 if user inputs "n"
    */
-  private static int roundCheck() {
+  private static int roundInputCheck() {
     // Until valid user input
     while (true) {
       // Outputs round check message
@@ -204,6 +361,7 @@ public class Baccarat {
       }
       // If user supplied an invalid input
       else {
+        // Outputs error message
         System.out.println("Please enter a valid input.");
       }
     }
@@ -215,7 +373,7 @@ public class Baccarat {
    * @return 1 if less than 6 cards or if user wants to stop in interactive mode
    *   0 if 6 or more cards and user wants to continnue in interactive mode 
    */
-  private static int shoeSizeCheck() {
+  private static int roundContinueCheck() {
     // If there are less than 6 cards remaining in the shoe
     if (baccaratShoe.size() < 6) {
       // Ends the game
@@ -224,7 +382,7 @@ public class Baccarat {
     // If there are at least 6 cards in the shoe
     else {
       // Checks for interactive mode, if yes then checks if user wants to continue
-      if (interactiveMode == 1 && roundCheck() == 0) {
+      if (interactiveMode == 1 && roundInputCheck() == 0) {
         // Ends the game
         return 0;
       }
@@ -269,31 +427,41 @@ public class Baccarat {
   public static void main(String[] args) {
     // Checks for interactive mode
     interactiveModeCheck(args);
-
     // Uses fancy symbols for suit, comment if you would like to use regular letters
     useFancySymbols();
-
     // Initialises game
     gameInitialisation();
     
+    // Main game program
     do {
       // Processes new round
       newRound();
-
-      // Shuffles shoe
-      baccaratShoe.shuffle();
-
       // Deals cards to player and banker
       dealCards();
-
       // Displays player and banker hands and their values
       handDisplay();
+
+      // If neither hand was natural, consults tableau
+      if (naturalCheck() == 0) {
+        // First consults player's rules
+        playersRule();
+        // Secondly consults banker's rules
+        bankersRule();
+        // Displays player and banker hands and their values
+        handDisplay();
+        // If neither hand is a natural again
+        if (naturalCheck() == 0) {
+          // Goes to final scoring
+          finalScoring();
+        }
+      }
     }
-    while (shoeSizeCheck() == 1);
+    // If there are sufficient cards in the shoe
+    // If user wants to continue in interactive mode
+    while (roundContinueCheck() == 1);
     
     // Outputs final game statistics
     gameDisplay();
-
     // Closes the scanner if in interactive mode
     scannerClose();
   }
