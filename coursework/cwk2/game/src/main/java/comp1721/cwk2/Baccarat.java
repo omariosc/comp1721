92b0a682bd36a76255b0d066643f9bb42acaabee
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-16 01:08:53 +0100

    Initial implementation on Full Solution Method for game initialisation Method for end of round display Method for end of game display Creates scanner for round check Method for checking if user wants to continue or not

--- coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
+++ coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
@@ -1,36 +1,105 @@
 package comp1721.cwk2;
 
+import java.util.Scanner;
+
 public class Baccarat {
-  public static void main(String[] args) {
 
-    // Creates and initialises Baccarat hands and shoe
-    var playerHand = new BaccaratHand();
-    var bankerHand = new BaccaratHand();
-    var baccaratShoe = new Shoe(6);
+  // Declares Baccarat hands and shoe
+  private BaccaratHand playerHand;
+  private BaccaratHand bankerHand;
+  private Shoe baccaratShoe;
 
-    // Shuffles shoe and deals two cards to both player and banker
-    baccaratShoe.shuffle();
-    playerHand.add(baccaratShoe.deal());
-    playerHand.add(baccaratShoe.deal());
-    bankerHand.add(baccaratShoe.deal());
-    bankerHand.add(baccaratShoe.deal());
-    
-    // Stores string representation of each hand
-    var playerString = playerHand.toString();
-    var bankerString = bankerHand.toString();
+  // Values used to record game statistics
+  private int rounds = 0;
+  private int roundsPlayer = 0;
+  private int roundsBanker = 0;
+  private int roundsTies = 0;
 
-    // Calculates value for hands
-    var playerValue = playerHand.value();
-    var bankerValue = bankerHand.value();
+  // Value used to check for interactive mode
+  private int interactiveMode = 0;
 
-    // Outputs hands and message if either or both hands are naturals    
-    System.out.printf("Player: %s = %d%n", playerString, playerValue);
-    System.out.printf("Banker: %s = %d%n", bankerString, bankerValue);
-    if (playerHand.isNatural()) {
-      System.out.println("Player has a Natural");
+  // Creates scanner for input in interactive mode
+  private Scanner input;
+
+  /**
+   * Initialises Baccarat hands and shoe
+   */
+  public void gameInitialisation() {
+    // Initialises player and banker hands
+    playerHand = new BaccaratHand();
+    bankerHand = new BaccaratHand();
+    // Initialises shoe
+    baccaratShoe = new Shoe(6);
+    // Creates scanner
+    input = new Scanner(System.in);
+  }
+
+  /**
+   * Outputs string representation and value of each hand
+   */
+  public void roundDisplay() {
+    // Outputs hands and message if either or both hands are naturals
+    System.out.printf("Player: %s = %d%n", playerHand.toString(), playerHand.value());
+    System.out.printf("Banker: %s = %d%n", bankerHand.toString(), bankerHand.value());
+  }
+
+  /**
+   * Outputs message for round check and processes user input
+   * Continues until user inputs valid input
+   * 
+   * @return 1 if user inputs "y", 0 if user inputs "n"
+   */
+  public int roundCheck() {
+    // Until valid user input
+    while (true) {
+      // Outputs round check message
+      System.out.print("Another round? (y/n): ");
+      // Stores user input
+      String userInput = input.next();
+      // Checks if input is equal to "n"
+      if (userInput.equals("n")) {
+        return 0;
+      }
+      else if (userInput.equals("y")) {
+        return 1;
+      }
+      else {
+        System.out.println("Please enter a valid input.");
+      }
     }
-    if (bankerHand.isNatural()) {
-      System.out.println("Banker has a Natural");
-    } 
+  }
+
+  /**
+   * Outputs final game statistics
+   */
+  public void gameDisplay() {
+    System.out.printf("%d rounds played %n", rounds);
+    System.out.printf("%d player wins %n", roundsPlayer);
+    System.out.printf("%d banker wins %n", roundsBanker);
+    System.out.printf("%d ties", roundsTies);
+  }
+
+  /**
+   * Program to play a complete game of Baccarat
+   * 
+   * @param args Used to check if user wants to play using interactive mode
+   */
+  public void main(String[] args) {
+    // If program is run with the "-i" option specified on the command line
+    if (args[0] != null && args[0].equals("-i")) {
+      // Enables interactive mode
+      interactiveMode = 1;
+    }
+
+    // Initialises game
+    gameInitialisation();
+
+    
+
+    // Outputs final game statistics
+    gameDisplay();
+
+    // Closes the scanner
+    input.close();
   }
 }
