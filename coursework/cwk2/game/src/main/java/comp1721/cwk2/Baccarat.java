6a18b1a172f263ec5d496a27233a4bf74c4a54bf
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-16 17:04:16 +0100

    Method for checking interactive mode Method for using unicode symbols for suit Only creates and closes scanner in interactive mode roundDisplay now outputs round number handDisplay now implements previous roundDisplay roundCheck swaps return value Method to check shoe size shoeSizeCheck processes user input in interactive mode Method to close the scanner in interactive mode Added more comments Main now plays the game without scoring

--- coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
+++ coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
@@ -5,39 +5,82 @@ import java.util.Scanner;
 public class Baccarat {
 
   // Declares Baccarat hands and shoe
-  private BaccaratHand playerHand;
-  private BaccaratHand bankerHand;
-  private Shoe baccaratShoe;
+  private static BaccaratHand playerHand;
+  private static BaccaratHand bankerHand;
+  private static Shoe baccaratShoe;
 
   // Values used to record game statistics
-  private int rounds = 0;
-  private int roundsPlayer = 0;
-  private int roundsBanker = 0;
-  private int roundsTies = 0;
+  private static int rounds = 0;
+  private static int roundsPlayer = 0;
+  private static int roundsBanker = 0;
+  private static int roundsTies = 0;
 
   // Value used to check for interactive mode
-  private int interactiveMode = 0;
+  private static int interactiveMode = 0;
 
   // Creates scanner for input in interactive mode
-  private Scanner input;
+  private static Scanner input;
+
+  /**
+   * Checks if the user wants to play interactive mode
+   * 
+   * @param args Used to check if user wants to play using interactive mode
+   * @return 1 if user specified interactive mode, 0 otherwise
+   */
+  private static void interactiveModeCheck(String[] args) {
+    // If program is run with the "-i" option specified on the command line
+    if (args.length != 0 && args[0] != null && args[0].equals("-i")) {
+      // Enables interactive mode
+      interactiveMode = 1;
+    }
+  }
+
+  /**
+   * Uses unicode symbols for card suit
+   */
+  private static void useFancySymbols() {
+    // Uses fancy symbols
+    Card.useFancySymbols(true);
+  }
 
   /**
    * Initialises Baccarat hands and shoe
    */
-  public void gameInitialisation() {
+  private static void gameInitialisation() {
     // Initialises player and banker hands
     playerHand = new BaccaratHand();
     bankerHand = new BaccaratHand();
     // Initialises shoe
     baccaratShoe = new Shoe(6);
-    // Creates scanner
-    input = new Scanner(System.in);
+    // If in interactive mode
+    if (interactiveMode == 1) {
+      // Creates scanner
+      input = new Scanner(System.in);
+    }
+  }
+
+  /**
+   * Displays the round number message
+   */
+  private static void roundDisplay() {
+    rounds++;
+    System.out.printf("%nRound %d%n", rounds);
+  }
+
+  /**
+   * Deals two cards to player and banker
+   */
+  private static void dealCards() {
+    playerHand.add(baccaratShoe.deal());
+    playerHand.add(baccaratShoe.deal());
+    bankerHand.add(baccaratShoe.deal());
+    bankerHand.add(baccaratShoe.deal());
   }
 
   /**
    * Outputs string representation and value of each hand
    */
-  public void roundDisplay() {
+  private static void handDisplay() {
     // Outputs hands and message if either or both hands are naturals
     System.out.printf("Player: %s = %d%n", playerHand.toString(), playerHand.value());
     System.out.printf("Banker: %s = %d%n", bankerHand.toString(), bankerHand.value());
@@ -49,20 +92,24 @@ public class Baccarat {
    * 
    * @return 1 if user inputs "y", 0 if user inputs "n"
    */
-  public int roundCheck() {
+  private static int roundCheck() {
     // Until valid user input
     while (true) {
       // Outputs round check message
       System.out.print("Another round? (y/n): ");
       // Stores user input
       String userInput = input.next();
-      // Checks if input is equal to "n"
-      if (userInput.equals("n")) {
-        return 0;
-      }
-      else if (userInput.equals("y")) {
+      // Checks if input is equal to "y"
+      if (userInput.equals("y")) {
+        // Game continues
         return 1;
       }
+      // Checks if input is equal to "n"
+      else if (userInput.equals("n")) {
+        // Ends the round
+        return 0;
+      }
+      // If user supplied an invalid input
       else {
         System.out.println("Please enter a valid input.");
       }
@@ -70,13 +117,55 @@ public class Baccarat {
   }
 
   /**
+   * Checks size of shoe and asks user for continuation if in interactive mode
+   * 
+   * @return 1 if less than 6 cards or if user wants to stop in interactive mode
+   *   0 if 6 or more cards and user wants to continnue in interactive mode 
+   */
+  private static int shoeSizeCheck() {
+    // If there are less than 6 cards remaining in the shoe
+    if (baccaratShoe.size() < 6) {
+      // Ends the game
+      return 0;
+    }
+    // If there are at least 6 cards in the shoe
+    else {
+      // Checks for interactive mode, if yes then checks if user wants to continue
+      if (interactiveMode == 1 && roundCheck() == 0) {
+        // Ends the game
+        return 0;
+      }
+      // If in default mode or if user does not want to continue
+      else {
+        // Game continues
+        return 1;
+      }
+    }
+  }
+
+  /**
    * Outputs final game statistics
    */
-  public void gameDisplay() {
-    System.out.printf("%d rounds played %n", rounds);
+  private static void gameDisplay() {
+    // Displays number of rounds played
+    System.out.printf("%n%d rounds played %n", rounds);
+    // Displays number of player wins
     System.out.printf("%d player wins %n", roundsPlayer);
+    // Displays number of banker wins
     System.out.printf("%d banker wins %n", roundsBanker);
+    // Displays number of ties
     System.out.printf("%d ties", roundsTies);
+  }
+
+  /**
+   * Closes scanner if in interactive mode
+   */
+  private static void scannerClose() {
+    // If in interactive mode
+    if (interactiveMode == 1) {
+      // Closes the scanner
+      input.close();
+    }
   }
 
   /**
@@ -84,22 +173,35 @@ public class Baccarat {
    * 
    * @param args Used to check if user wants to play using interactive mode
    */
-  public void main(String[] args) {
-    // If program is run with the "-i" option specified on the command line
-    if (args[0] != null && args[0].equals("-i")) {
-      // Enables interactive mode
-      interactiveMode = 1;
-    }
+  public static void main(String[] args) {
+    // Checks for interactive mode
+    interactiveModeCheck(args);
+
+    // Uses fancy symbols for suit, comment if you would like to use regular letters
+    // useFancySymbols();
 
     // Initialises game
     gameInitialisation();
-
     
+    do {
+      // Displays rounds number message
+      roundDisplay();
 
+      // Shuffles shoe and deals two cards to both player and banker
+      baccaratShoe.shuffle();
+
+      // Deals cards to player and banker
+      dealCards();
+
+      // Displays player and banker hands and their values
+      handDisplay();
+    }
+    while (shoeSizeCheck() == 1);
+    
     // Outputs final game statistics
     gameDisplay();
 
-    // Closes the scanner
-    input.close();
+    // Closes the scanner if in interactive mode
+    scannerClose();
   }
 }
