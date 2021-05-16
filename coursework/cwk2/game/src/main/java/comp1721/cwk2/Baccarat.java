b40928760ce81ee7746cdf560ecfeb9ecf49d5b1
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-05-16 18:09:52 +0100

    Method to initialise shoe Prompts user for number of decks to use in interactive mode Uses 6 decks in default mode Discards hands in new round Methods to process player or banker win Methods to give third card to player and/or banker

--- coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
+++ coursework/cwk2/game/src/main/java/comp1721/cwk2/Baccarat.java
@@ -44,27 +44,80 @@ public class Baccarat {
   }
 
   /**
+   * Initialises number of decks in the shoe
+   * 
+   * <p>Below commented implementation hard codes and uses 6 as the number of decks
+   * 
+   *    <code>baccaratShoe = new Shoe(6);</code>
+   * 
+   * Meaning that the game will not be able to use 8 decks
+   * However, there are two ways to let the user specify the number of decks:
+   * 
+   * 1. User specified number of decks in the command line (processing any errors too)
+   * 
+   *    <code>if (args != null && args[1] != null & (args[1].equals("6") || args[1].equals("8"))
+   *            { baccaratShoe = new Shoe(args[1]); }</code>
+   * 
+   * 2. Prompt user for input for the number of the decks in interactive mode
+   *    Use hard coded value in default mode
+   * 
+   * I decided to go with implementation number 2 in which the user
+   * chooses the number of decks they would like to play with in interactive mode,
+   * since in the default mode, no command line arguments are passed (as per coursework specification)
+   * and the functionality is essentialy the same.
+   * 
+   * Therefore in default mode, the shoe contains 6 decks. Of course 
+   * this has its own limitations in that in default mode the user cannot play with 8 decks.
+   * 
+   * This option can be disabled by commenting out the <code>while</code> loop</p>
+   */
+  private static void initialiseShoe() {
+    // Initially hard codes number of decks
+    var decks = 6;
+    // If interactive mode is on
+    while (interactiveMode == 1) {
+      // Outputs number of decks prompt
+      System.out.print("Enter number of decks you would like to play with (6/8): ");
+      // Stores user input
+      String userInput = input.next();
+      // Checks if input is valid number of decks
+      if (userInput.equals("6") || userInput.equals("8")) {
+        // Sets number of decks to userInput
+        decks = Integer.parseInt(userInput);
+        break;
+      }
+      // If user supplied an invalid input
+      else {
+        System.out.println("Please enter a valid input.");
+      }
+    }
+    baccaratShoe = new Shoe(decks);
+  }
+
+  /**
    * Initialises Baccarat hands and shoe
    */
   private static void gameInitialisation() {
     // Initialises player and banker hands
     playerHand = new BaccaratHand();
     bankerHand = new BaccaratHand();
-    // Initialises shoe
-    baccaratShoe = new Shoe(6);
     // If in interactive mode
     if (interactiveMode == 1) {
       // Creates scanner
       input = new Scanner(System.in);
     }
+    // Initialises shoe
+    initialiseShoe();
   }
 
   /**
-   * Displays the round number message
+   * Displays the round number message and clears hand
    */
-  private static void roundDisplay() {
+  private static void newRound() {
     rounds++;
     System.out.printf("%nRound %d%n", rounds);
+    playerHand.discard();
+    bankerHand.discard();
   }
 
   /**
@@ -84,6 +137,46 @@ public class Baccarat {
     // Outputs hands and message if either or both hands are naturals
     System.out.printf("Player: %s = %d%n", playerHand.toString(), playerHand.value());
     System.out.printf("Banker: %s = %d%n", bankerHand.toString(), bankerHand.value());
+  }
+
+  /**
+   * Processes player win
+   */
+  private static void playerWin() {
+    // Increments number of rounds player has won
+    roundsPlayer++;
+    // Outputs player win message
+    System.out.println("Player win!");
+  }
+
+  /**
+   * Processes banker win
+   */
+  private static void bankerWin() {
+    // Increments number of rounds banker has won
+    roundsBanker++;
+    // Outputs banker win message
+    System.out.println("Banker win!");
+  }
+
+  /**
+   * Deals third card to player and outputs dealing message
+   */
+  private static void dealThirdCardPlayer() {
+    // Outputs dealing message
+    System.out.println("Dealing third card to player...");
+    // Adds card to player hand from game shoe
+    playerHand.add(baccaratShoe.deal());
+  }
+
+  /**
+   * Deals third card to banker and outputs dealing message
+   */
+  private static void dealThirdCardBanker() {
+    // Outputs dealing message
+    System.out.println("Dealing third card to banker...");
+    // Adds card to banker hand from game shoe
+    bankerHand.add(baccaratShoe.deal());
   }
 
   /**
@@ -178,16 +271,16 @@ public class Baccarat {
     interactiveModeCheck(args);
 
     // Uses fancy symbols for suit, comment if you would like to use regular letters
-    // useFancySymbols();
+    useFancySymbols();
 
     // Initialises game
     gameInitialisation();
     
     do {
-      // Displays rounds number message
-      roundDisplay();
+      // Processes new round
+      newRound();
 
-      // Shuffles shoe and deals two cards to both player and banker
+      // Shuffles shoe
       baccaratShoe.shuffle();
 
       // Deals cards to player and banker
